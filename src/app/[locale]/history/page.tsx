ae3dab888a00f8e8dad9426060eae9cd7fc11ac6
Yurei TZK <me@yureitzk.com>
2026-01-08 22:01:39 +0300

    Add debounce to history search input

--- src/app/[locale]/history/page.tsx
+++ src/app/[locale]/history/page.tsx
@@ -7,15 +7,19 @@ import Channels from '@/app/components/features/channel/Channels';
 import DeleteAllChannelsButton from '@/app/components/features/history/DeleteAllChannelsButton';
 import { useStore } from '@hooks/useStore';
 import SearchChannelInput from '@/app/components/features/history/SearchChannelInput';
+import { useDebounce } from '@hooks/useDebounce';
 
 export default function History() {
 	const t = useTranslations('History');
 	const [currentPage, setCurrentPage] = useState(1);
 	const [searchQuery, setSearchQuery] = useState('');
-	const { countAllChannels } = useStore();
 
+	const debouncedSearchQuery = useDebounce(searchQuery, 200);
+
+	const { countAllChannels } = useStore();
 	const totalChannelsCount = countAllChannels() || 0;
-	const filteredChannelsCount = countAllChannels(searchQuery) || 0;
+
+	const filteredChannelsCount = countAllChannels(debouncedSearchQuery) || 0;
 
 	const channelsLimit = 5;
 	const channelsTotal =
@@ -31,7 +35,7 @@ export default function History() {
 
 	useEffect(() => {
 		setCurrentPage(1);
-	}, [searchQuery]);
+	}, [debouncedSearchQuery]);
 
 	const hasChannels = totalChannelsCount > 0;
 
@@ -39,7 +43,6 @@ export default function History() {
 		<>
 			<h2 className='font-semibold'>{t('title')}</h2>
 			<p className='mb-4'>{t('description')}</p>
-
 			{hasChannels && (
 				<>
 					<SearchChannelInput
@@ -50,11 +53,10 @@ export default function History() {
 					<DeleteAllChannelsButton className='mb-5' />
 				</>
 			)}
-
 			{filteredChannelsCount > 0 ? (
 				<>
 					<Channels
-						searchQuery={searchQuery}
+						searchQuery={debouncedSearchQuery}
 						pageNumber={currentPage}
 						pageSize={channelsLimit}
 						className='mb-5'
@@ -65,18 +67,14 @@ export default function History() {
 						onChange={setCurrentPage}
 					/>
 				</>
-			) : hasChannels && searchQuery ? (
+			) : hasChannels && debouncedSearchQuery ? (
 				<p className='text-center text-gray-600/90 dark:text-gray-300 py-8'>
-					{t('noResultsFound', { query: searchQuery })}
+					{t('noResultsFound')}
 				</p>
 			) : !hasChannels ? (
 				<div className='text-center py-6'>
-					<p className='mb-1'>
-						{t('emptyState.title')}
-					</p>
-					<p className='text-sm'>
-						{t('emptyState.description')}
-					</p>
+					<p className='mb-1'>{t('emptyState.title')}</p>
+					<p className='text-sm'>{t('emptyState.description')}</p>
 				</div>
 			) : null}
 		</>

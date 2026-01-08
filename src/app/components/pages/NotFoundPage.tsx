e4dee07fc78f92c1e39e452c956362d46c71998a
Yurei TZK <me@yureitzk.com>
2026-01-08 21:55:57 +0300

    Update NotFoundPage component

--- src/app/components/pages/NotFoundPage.tsx
+++ src/app/components/pages/NotFoundPage.tsx
@@ -6,7 +6,7 @@ export default function NotFoundPage() {
 	return (
 		<>
 			<h1 className='text-center mx-auto mb-3'>{t('title')}</h1>
-			<p className='max-w-[460px] mx-auto text-center'>{t('description')}</p>
+			<p className='max-w-[460px] mx-auto text-center text-sm'>{t('description')}</p>
 		</>
 	);
 }
